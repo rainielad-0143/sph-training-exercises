e9c93db02f248d7651032b6f0eea2285ee59d40c
Rainiel Dejito <rainiel.dejito@sun-asterisk.com>
2026-04-23 18:14:54 +0800

    Added queries
    * Create a database library_db
    
    * Added newline at the end of file
    
    * Test newline
    
    * Added newline at the end of the file
    
    * Created tables
    
    * Added line break
    
    * Created tables
    
    * Inserted values to the tables
    
    * Added line break
    
    * Added queries
    
    * Added line break
    
    * Added new values for authors and books

--- exercise-03-mysql-library/submissions/dejito_rainiel.sql
+++ exercise-03-mysql-library/submissions/dejito_rainiel.sql
@@ -32,14 +32,16 @@ CREATE TABLE borrow_records (
 INSERT INTO authors (author_name) VALUES
 ('Ali Hazelwood'),
 ('TurtleMe'),
-('Shing-Shong');
+('Shing-Shong'),
+('Jee Gab Song');
 
 INSERT INTO books (title, author_id) VALUES
 ('The Love Hypothesis', 1),
 ('Love on the Brain', 1),
 ('The Beginning After The End', 2),
 ('Omniscient Reader''s Viewpoint', 3),
-('The World After The Fall', 3);
+('The World After The Fall', 3),
+('The Novel''s Extra', 4);
 
 INSERT INTO members (member_name) VALUES
 ('Adam'),
@@ -53,4 +55,34 @@ INSERT INTO borrow_records (member_id, book_id, borrow_date, return_date) VALUES
 (4, 2, '2026-04-05', '2026-04-14'),
 (2, 3, '2026-04-07', NULL),
 (3, 5, '2026-04-09', NULL),
-(5, 4, '2026-04-09', NULL);
+(5, 4, '2026-04-09', NULL),
+(4, 6, '2026-04-09', NULL);
+
+SELECT books.title, authors.author_name
+FROM books
+JOIN authors ON books.author_id = authors.id;
+
+SELECT members.member_name, books.title, borrow_records.borrow_date
+FROM borrow_records
+JOIN members ON borrow_records.member_id = members.id
+JOIN books ON borrow_records.book_id = books.id
+WHERE borrow_records.return_date IS NULL;
+
+SELECT DISTINCT members.member_name
+FROM borrow_records
+JOIN members ON borrow_records.member_id = members.id
+JOIN books ON borrow_records.book_id = books.id
+JOIN authors ON books.author_id = authors.id
+WHERE authors.author_name = 'Ali Hazelwood';
+
+SELECT authors.author_name, COUNT(books.id) AS total_books
+FROM authors
+LEFT JOIN books ON books.author_id = authors.id
+GROUP BY authors.id, authors.author_name;
+
+SELECT members.member_name, books.title, borrow_records.borrow_date, borrow_records.return_date
+FROM borrow_records
+JOIN members ON borrow_records.member_id = members.id
+JOIN books ON borrow_records.book_id = books.id
+WHERE members.member_name = 'Dokja'
+ORDER BY borrow_records.borrow_date;
