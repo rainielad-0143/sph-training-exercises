fbfb377bc70f5a32b60f011be65cf21f2673cef8
Rainiel Dejito <rainiel.dejito@sun-asterisk.com>
2026-04-23 17:41:46 +0800

    Inserted values to the tables
    * Create a database library_db
    
    * Added newline at the end of file
    
    * Test newline
    
    * Added newline at the end of the file
    
    * Created tables
    
    * Added line break
    
    * Created tables
    
    * Inserted values to the tables
    
    * Added line break

--- exercise-03-mysql-library/submissions/dejito_rainiel.sql
+++ exercise-03-mysql-library/submissions/dejito_rainiel.sql
@@ -28,3 +28,29 @@ CREATE TABLE borrow_records (
     FOREIGN KEY (member_id) REFERENCES members(id),
     FOREIGN KEY (book_id) REFERENCES books(id)
 );
+
+INSERT INTO authors (author_name) VALUES
+('Ali Hazelwood'),
+('TurtleMe'),
+('Shing-Shong');
+
+INSERT INTO books (title, author_id) VALUES
+('The Love Hypothesis', 1),
+('Love on the Brain', 1),
+('The Beginning After The End', 2),
+('Omniscient Reader''s Viewpoint', 3),
+('The World After The Fall', 3);
+
+INSERT INTO members (member_name) VALUES
+('Adam'),
+('Arthur'),
+('Jaewan'),
+('Dokja'),
+('Joong-Hyuk');
+
+INSERT INTO borrow_records (member_id, book_id, borrow_date, return_date) VALUES
+(1, 1, '2026-04-01', '2026-04-10'),
+(4, 2, '2026-04-05', '2026-04-14'),
+(2, 3, '2026-04-07', NULL),
+(3, 5, '2026-04-09', NULL),
+(5, 4, '2026-04-09', NULL);
