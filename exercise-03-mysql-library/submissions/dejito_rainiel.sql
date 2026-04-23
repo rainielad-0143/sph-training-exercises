9e56184f1937af08724c4b1c0c2b0f7c9cb69e77
Rainiel Dejito <rainiel.dejito@sun-asterisk.com>
2026-04-23 16:45:55 +0800

    Created tables
    * Create a database library_db
    
    * Added newline at the end of file
    
    * Test newline
    
    * Added newline at the end of the file
    
    * Created tables
    
    * Added line break
    
    * Created tables

--- exercise-03-mysql-library/submissions/dejito_rainiel.sql
+++ exercise-03-mysql-library/submissions/dejito_rainiel.sql
@@ -1,1 +1,30 @@
 CREATE DATABASE library_db;
+
+USE library_db;
+
+CREATE TABLE authors (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    author_name VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE books (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    author_id INT NOT NULL,
+    FOREIGN KEY (author_id) REFERENCES authors(id)
+);
+
+CREATE TABLE members (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    member_name VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE borrow_records (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    member_id INT NOT NULL,
+    book_id INT NOT NULL,
+    borrow_date DATE NOT NULL,
+    return_date DATE NULL,
+    FOREIGN KEY (member_id) REFERENCES members(id),
+    FOREIGN KEY (book_id) REFERENCES books(id)
+);
