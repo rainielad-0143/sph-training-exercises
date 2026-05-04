e6db699ab1df14b6c8b68b2a3b7ca49dc209c143
Rainiel Dejito <rainiel.dejito@sun-asterisk.com>
2026-05-04 11:55:21 +0800

    Added filter by category query

--- exercise-04-prisma-library/submissions/dejito_rainiel/index.ts
+++ exercise-04-prisma-library/submissions/dejito_rainiel/index.ts
@@ -1,39 +1,57 @@
 import { prisma } from "./lib/prisma";
 
 async function main() {
-  const [allPosts, publishedPosts, userPosts, usersWithPostCount] =
-    await Promise.all([
-      prisma.post.findMany({
-        include: {
-          author: true,
+  const [
+    allPosts,
+    publishedPosts,
+    userPosts,
+    usersWithPostCount,
+    filterCategories,
+  ] = await Promise.all([
+    prisma.post.findMany({
+      include: {
+        author: true,
+      },
+    }),
+    prisma.post.findMany({
+      where: {
+        published: true,
+      },
+    }),
+    prisma.post.findMany({
+      where: {
+        author: {
+          name: "Jinhyuk",
         },
-      }),
-      prisma.post.findMany({
-        where: {
-          published: true,
-        },
-      }),
-      prisma.post.findMany({
-        where: {
-          author: {
-            name: "Jinhyuk",
+      },
+    }),
+    prisma.user.findMany({
+      include: {
+        _count: {
+          select: {
+            posts: true,
           },
         },
-      }),
-      prisma.user.findMany({
-        include: {
-          _count: {
-            select: {
-              posts: true,
-            },
+      },
+    }),
+    prisma.post.findMany({
+      where: {
+        category: {
+          name: {
+            in: ["Tech", "Travel"],
           },
         },
-      }),
-    ]);
+      },
+      include: {
+        category: true,
+      },
+    }),
+  ]);
   console.log("All Posts:", allPosts);
   console.log("Published Posts:", publishedPosts);
-  console.log("User's Posts", userPosts);
-  console.log("Posts per User", usersWithPostCount);
+  console.log("User's Posts:", userPosts);
+  console.log("Posts per User:", usersWithPostCount);
+  console.log("Filtered Posts:", filterCategories);
 }
 
 main()
