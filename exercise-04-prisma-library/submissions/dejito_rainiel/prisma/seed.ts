b6f6984fb2e983110e0c546500517b245c5b254e
Rainiel Dejito <rainiel.dejito@sun-asterisk.com>
2026-05-04 11:35:25 +0800

    Added optional feature Category model linked to posts
    * Added optional feature Category model linked to posts
    
    * Changed category to categories for a clear and consistent name

--- exercise-04-prisma-library/submissions/dejito_rainiel/prisma/seed.ts
+++ exercise-04-prisma-library/submissions/dejito_rainiel/prisma/seed.ts
@@ -18,37 +18,56 @@ async function main() {
     ],
   });
 
+  const categories = await prisma.category.createManyAndReturn({
+    data: [
+      {
+        name: "Tech",
+      },
+      {
+        name: "Food",
+      },
+      {
+        name: "Travel",
+      },
+    ],
+  });
+
   await prisma.post.createMany({
     data: [
       {
         title: "Alice Post 1",
         content: "Hello from Alice",
         published: true,
-        userId: users[1].id,
+        userId: users[0].id,
+        categoryId: categories[0].id,
       },
       {
         title: "Alice Post 2",
         content: "Another post by Alice",
         published: false,
-        userId: users[1].id,
+        userId: users[0].id,
+        categoryId: categories[1].id,
       },
       {
         title: "Jinhyuk Post 1",
         content: "Jinhyuk's first post",
         published: true,
-        userId: users[2].id,
+        userId: users[1].id,
+        categoryId: categories[1].id,
       },
       {
         title: "Jinhyuk Post 2",
         content: "Jinhyuk again",
         published: false,
-        userId: users[2].id,
+        userId: users[1].id,
+        categoryId: categories[0].id,
       },
       {
         title: "Teresa Post 1",
         content: "Teresa here",
         published: true,
-        userId: users[3].id,
+        userId: users[2].id,
+        categoryId: categories[2].id,
       },
     ],
   });
