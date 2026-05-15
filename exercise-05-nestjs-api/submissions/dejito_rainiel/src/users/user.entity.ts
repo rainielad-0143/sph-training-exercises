faf142c7b18ffb5b5ecf312f1945d03f2cff9f9e
Rainiel <rainiel.dejito@sun-asterisk.com>
2026-05-15 11:19:02 +0800

    Generated user

---
+++ exercise-05-nestjs-api/submissions/dejito_rainiel/src/users/user.entity.ts
@@ -0,0 +1,13 @@
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column({ unique: true })
+  email: string;
+}
