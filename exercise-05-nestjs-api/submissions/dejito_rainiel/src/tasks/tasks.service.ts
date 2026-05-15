cc3234832dbc3dcdb0e92115847c3951703efb5e
Rainiel <rainiel.dejito@sun-asterisk.com>
2026-05-15 14:15:32 +0800

    Partial commit

--- exercise-05-nestjs-api/submissions/dejito_rainiel/src/tasks/tasks.service.ts
+++ exercise-05-nestjs-api/submissions/dejito_rainiel/src/tasks/tasks.service.ts
@@ -72,4 +72,11 @@ export class TasksService {
       message: 'Task deleted',
     };
   }
+
+  findByStatus(status: TaskStatus) {
+    return this.taskRepo.find({
+      where: { status },
+      relations: ['user'],
+    });
+  }
 }
