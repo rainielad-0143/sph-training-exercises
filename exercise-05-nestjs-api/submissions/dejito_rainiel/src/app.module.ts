92fbf7d00082a842877cfca6851badad259cf070
Rainiel <rainiel.dejito@sun-asterisk.com>
2026-05-15 11:42:32 +0800

    Added task entity and corresponding files

--- exercise-05-nestjs-api/submissions/dejito_rainiel/src/app.module.ts
+++ exercise-05-nestjs-api/submissions/dejito_rainiel/src/app.module.ts
@@ -5,19 +5,23 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 import { UsersModule } from './users/users.module';
+import { TasksModule } from './tasks/tasks.module';
 
 import { User } from './users/user.entity';
+import { Task } from './tasks/task.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'sqlite',
       database: 'db.sqlite',
-      entities: [User],
+      entities: [User, Task],
       synchronize: true,
     }),
 
     UsersModule,
+
+    TasksModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ exercise-05-nestjs-api/submissions/dejito_rainiel/src/tasks/task.entity.ts
@@ -0,0 +1,24 @@
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { User } from '../users/user.entity';
+import { TaskStatus } from './enums/task-status.enum';
+
+@Entity()
+export class Task {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column()
+  description: string;
+
+  @Column({
+    type: 'text',
+    default: TaskStatus.PENDING,
+  })
+  status: TaskStatus;
+
+  @ManyToOne(() => User, (user) => user.tasks)
+  userId: User;
+}
