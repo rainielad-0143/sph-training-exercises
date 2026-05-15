826ca401451d34f6eb91abe206e4b2a7e8588ba3
Rainiel Dejito <rainiel.dejito@sun-asterisk.com>
2026-05-15 13:36:45 +0800

    Added user entity and corresponding files
    * Generated user
    
    * Removed unused decorator

--- exercise-05-nestjs-api/submissions/dejito_rainiel/src/app.module.ts
+++ exercise-05-nestjs-api/submissions/dejito_rainiel/src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { UsersModule } from './users/users.module';
+
+import { User } from './users/user.entity';
+
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: [User],
+      synchronize: true,
+    }),
+
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
