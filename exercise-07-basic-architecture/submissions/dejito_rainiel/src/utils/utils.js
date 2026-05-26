174ad1ff357a72c914e6fae0420f4561da66c84a
Rainiel <rainiel.dejito@sun-asterisk.com>
2026-05-26 14:47:34 +0800

    Added OOP principle for BorrowedBooks

--- exercise-07-basic-architecture/submissions/dejito_rainiel/src/utils/utils.js
+++ exercise-07-basic-architecture/submissions/dejito_rainiel/src/utils/utils.js
@@ -3,12 +3,15 @@ export const formatDate = (date) => {
 };
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 export const isBookOverdue = (dueDate) => {
   return new Date(dueDate) < new Date();
 };
 
 >>>>>>> 00af33f (Added DRY principle in BorrowedBooks and MemberHistory)
+=======
+>>>>>>> 6c1dfe9 (Added OOP principle for BorrowedBooks)
 export function createMapById(array) {
   return Object.fromEntries(array.map((item) => [item.id, item]));
 }
